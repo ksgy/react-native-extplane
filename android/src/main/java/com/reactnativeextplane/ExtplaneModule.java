d5f7464d1d02a61807521d7ec9ef87a782a1c6d5
ksgy <ksgy@users.noreply.github.com>
2022-10-19 14:34:37 +0100

    Remove logger

--- android/src/main/java/com/reactnativeextplane/ExtplaneModule.java
+++ android/src/main/java/com/reactnativeextplane/ExtplaneModule.java
@@ -14,7 +14,7 @@ public class ExtplaneModule extends NativeExtplaneSpec {
     public ExtplaneModule(ReactApplicationContext reactContext) {
         super(reactContext);
     }
-    
+
     private ExtPlaneInterface iface = null;
 
     @Override

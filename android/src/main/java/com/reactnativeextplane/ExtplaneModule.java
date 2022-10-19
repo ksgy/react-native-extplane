f02d8218aff2d36f5696725e164c963893129f02
ksgy <ksgy@users.noreply.github.com>
2022-10-19 14:19:15 +0100

    Update ExtplaneModule.java

--- android/src/main/java/com/reactnativeextplane/ExtplaneModule.java
+++ android/src/main/java/com/reactnativeextplane/ExtplaneModule.java
@@ -6,7 +6,6 @@ import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.module.annotations.ReactModule;
-import org.cutre.soft.ExtPlaneInterface;
 
 @ReactModule(name = ExtplaneModule.NAME)
 public class ExtplaneModule extends NativeExtplaneSpec {
@@ -22,67 +21,67 @@ public class ExtplaneModule extends NativeExtplaneSpec {
         return NAME;
     }
 
-    @ReactMethod
-    public void start(String ip, int port) throws Exception {
-      iface = new ExtPlaneInterface(ip, port);
-      iface.start();
-    }
+     @Override
+     public void start(String ip, int port) throws Exception {
+       iface = new ExtPlaneInterface(ip, port);
+       iface.start();
+     }
 
-    @ReactMethod
-    public void includeDataRef(String dataref, Float precision) {
-      if (iface != null) {
-        iface.includeDataRef(dataref, precision);
-      }
-    }
+     @Override
+     public void includeDataRef(String dataref, Float precision) {
+       if (iface != null) {
+         iface.includeDataRef(dataref, precision);
+       }
+     }
 
-    @ReactMethod
-    public void excludeDataRef(String dataref) {
-      if (iface != null) {
-        iface.excludeDataRef(dataref);
-      }
-    }
+     @Override
+     public void excludeDataRef(String dataref) {
+       if (iface != null) {
+         iface.excludeDataRef(dataref);
+       }
+     }
 
-    @ReactMethod
-    public String[] getDataRefValue(String dataref) {
-      if (iface != null) {
-        return iface.getDataRefValue(dataref);
-      }
-      return new String[0];
-    }
+     @Override
+     public String[] getDataRefValue(String dataref) {
+       if (iface != null) {
+         return iface.getDataRefValue(dataref);
+       }
+       return new String[0];
+     }
 
-    @ReactMethod
-    public Constants.DataType getDataRefType(String dataref) {
-      if (iface != null) {
-        return iface.getDataRefType(dataref);
-      }
-      return null;
-    }
+     @Override
+     public Constants.DataType getDataRefType(String dataref) {
+       if (iface != null) {
+         return iface.getDataRefType(dataref);
+       }
+       return null;
+     }
 
-    @ReactMethod
-    public DataRef getDataRef(String dataref) {
-      if (iface != null) {
-        return iface.getDataRef(dataref);
-      }
-      return null;
-    }
+     @Override
+     public DataRef getDataRef(String dataref) {
+       if (iface != null) {
+         return iface.getDataRef(dataref);
+       }
+       return null;
+     }
 
-    @ReactMethod
-    public void observeDataRef(String dataref, Callback callback) {
-      if (iface != null) {
-        Observer<DataRef> s = new Observer<DataRef>() {
-          public void update(DataRef object) {
-            callback.invoke(object);
-          }
-        };
-        iface.observeDataRef(dataref, s);
-      }
-    }
+     @Override
+     public void observeDataRef(String dataref, Callback callback) {
+       if (iface != null) {
+         Observer<DataRef> s = new Observer<DataRef>() {
+           public void update(DataRef object) {
+             callback.invoke(object);
+           }
+         };
+         iface.observeDataRef(dataref, s);
+       }
+     }
 
-    //  @ReactMethod
-    //  public void unObserveDataRef(String dataref) {
-    //    if (iface != null) {
-    //      iface.unObserveDataRef(dataref);
-    //    }
-    //  }
-    }
+     @Override
+     public void unObserveDataRef(String dataref) {
+       if (iface != null) {
+         iface.unObserveDataRef(dataref);
+       }
+     }
+
 }
